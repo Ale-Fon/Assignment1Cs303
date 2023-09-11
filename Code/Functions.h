cb354aed805435d8f5896b01c5cd482196997efe
Ale-Fon <90851040+Ale-Fon@users.noreply.github.com>
2023-09-11 00:27:11 -0500

    Update Functions.h

--- Code/Functions.h
+++ Code/Functions.h
@@ -7,9 +7,8 @@ using namespace std;
 
 void getRow(ifstream &inFile, vector<vector<int>> &v, int input1, int input2);
 void printRow(vector<vector<int>> &v, int input1, int input2);
-void checkIndex(vector<vector<int>> &v, int index, int input1, int input2);
-void changeIndexValue(vector<vector<int>> &v, int index, int input1, int input2,
-                      int &tempNum);
-void adding(vector<vector<int>> &v, int index, int newNum);
-void deleting(vector<vector<int>> &v, int index, int size2);
-void transforming(int index, int &input1, int &input2);
+void checkValue(vector<vector<int>> &v, int value, int input1, int input2);
+void changeValue(vector<vector<int>> &v, int value, int input1, int input2, int &tempNum);
+void adding(vector<vector<int>> &v, int value, int newValue);
+void deleting(vector<vector<int>> &v, int value, int size2);
+void transforming(int value, int &input1, int &input2);
